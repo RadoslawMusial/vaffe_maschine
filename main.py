0ed8a12af658355ee4dae227990afebb862256a2
RadoslawMusial <onlynaves@gmail.com>
2024-07-14 02:09:44 +0200

    caffe maschine

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
-
 import report
-list = ["ekspresso", "report", "capuccio", "czarna"]
-ekspresso_1 = 0
+list = ["espresso", "report", "cappuccino", "black"]
+espresso_1 = 0
 
 while True:
-    print("Jak chcesz napój? ")
+    print("What drink would you like?")
     a = input()
     if a == "report":
         report.report()
 
     if a not in list:
-        print("tego nima, dawaj dalej")
+        print("We don't have that, try again")
     else:
         break
 
-if a == "ekspresso":
-    ekspresso_1 += 2
-    print("Wybrales ekspresso, czy chcesz z cukrem? (tak/nie)")
+if a == "espresso":
+    espresso_1 += 2
+    print("You chose espresso, would you like it with sugar? (yes/no)")
     state_1 = input()
-    if state_1 == "tak":
-        print("Wybrano ekspresso z cukrem")
-        ekspresso_1 += 2
-    elif state_1 == "nie":
-        print("Wybrano ekspresso bez cukru")
+    if state_1 == "yes":
+        print("Espresso with sugar chosen")
+        espresso_1 += 2
+    elif state_1 == "no":
+        print("Espresso without sugar chosen")
 
-    print("Czy chcesz ciasto do ekspresso? (tak/nie)")
+    print("Would you like a cake with your espresso? (yes/no)")
     state_2 = input()
-    if state_2 == "tak":
-        print("Jakie ciastko? Wybór to ptys i omaga")
+    if state_2 == "yes":
+        print("Which cake? Choices are eclair and macaron")
         state_3 = input()
-        if state_3 == "ptys":
-            ekspresso_1 += 2
-        elif state_3 == "omaga":
-            ekspresso_1 += 4
-    elif state_2 == "nie":
-        print("Wybrano ekspresso bez ciastka")
+        if state_3 == "eclair":
+            espresso_1 += 2
+        elif state_3 == "macaron":
+            espresso_1 += 4
+    elif state_2 == "no":
+        print("Espresso without cake chosen")
 elif a == "report":
-    print("Ekspresso 200ml")
-    print("Kawkiem inna 300ml")
+    print("Espresso 200ml")
+    print("Other coffee 300ml")
 else:
-    print("Kawkiem")
+    print("Coffee")
 
-    print(f"SIUPER nie bedzie pania, Twoje zamówienie wynosi {ekspresso_1}")
+print(f"GREAT, no problem, your order total is {espresso_1}")
